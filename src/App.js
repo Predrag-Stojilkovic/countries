085da2f2dab3e59743f687da8fc15448b0da4b3b
Predrag Stojilkovic <predrag.stojilkovic@gmail.com>
2021-02-17 21:11:34 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './app.css';
+
+function App() {    
+
+    return (
+        <>
+            <h1>Hello motherfuckers!</h1>
+        </>
+    )
+}
+
+export default App;
